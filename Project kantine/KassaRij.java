ef41af5fda405475ec333aff09d73ce085a29b45
jeroen bakker <jeroen.henk.bakker@hccnet.nl>
2014-11-30 16:26:50 +0100

    ...

---
+++ Project kantine/KassaRij.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class KassaRij here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KassaRij
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class KassaRij
+     */
+    public KassaRij()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    /**
+     * persoon sluit achter in de rij aan
+     * @param persoon
+     */
+    public void sluitAchteraan(Persoon persoon) {
+        //methode body omitted
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
