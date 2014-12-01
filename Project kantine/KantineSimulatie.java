51e27e34f14c2fcff5b5de13e720ba682f0c0a01
Bas de Ruiter <b.d.r._bas@live.nl>
2014-12-01 16:03:18 +0100

    -----------------

--- Project kantine/KantineSimulatie.java
+++ Project kantine/KantineSimulatie.java
@@ -7,30 +7,38 @@
  */
 
 public class KantineSimulatie {
- private Kantine kantine;
- /**
- * Constructor
- */
- public KantineSimulatie() {
- kantine=new Kantine();
- }
- /**
- * Deze methode simuleert een aantal dagen in het
- * verloop van de kantine
- * @param dagen
- */
-public void simuleer(int dagen) {
- // for lus voor dagen
- for(...) {
- // per dag nu even vast 10+i personen naar binnen
- // laten gaan, wordt volgende week veranderd...
- // for lus voor personen
- for(int j=0;j<10+i;j++){
- // kantine.(…);
- }
- // verwerk rij voor de kassa
- // toon dagtotalen (artikelen en geld in kassa)
- // reset de kassa voor de volgende dag
- }
- }
+     private Kantine kantine;
+     /**
+      * Constructor
+      */
+     public KantineSimulatie() {
+         kantine=new Kantine();
+     }
+     
+     /**
+      * Deze methode simuleert een aantal dagen in het
+      * verloop van de kantine
+      * @param dagen
+      */
+    public void simuleer(int dagen) {
+        // for lus voor dagen
+        for(int k=0; k <= dagen - 1; k++ ) {
+            // per dag nu even vast 10+i personen naar binnen
+            // laten gaan, wordt volgende week veranderd...
+            // for lus voor personen
+            for(int j=0;j<10+i;j++){
+                kantine.loopPakSluitAan();       
+            }
+            kantine.verwerkRijVoorKassa();
+            System.out.println("Dag " k + 1);
+            System.out.println("Dagtotalen:");
+            System.out.println("Totaal in kassa: " kantine.hoeveelheidGeldInKassa());
+            System.out.println("Totaal aantal artikelen: " kantine.aantalArtikelen())
+            
+            
+            // verwerk rij voor de kassa
+            // toon dagtotalen (artikelen en geld in kassa)
+            // reset de kassa voor de volgende dag
+        }
+     }
 }
