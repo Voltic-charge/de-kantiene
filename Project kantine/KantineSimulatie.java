77b05e86de118197ef56b1a88b5063e2d5caca96
Bas de Ruiter <b.d.r._bas@live.nl>
2014-12-01 14:58:01 +0100

    joooo

---
+++ Project kantine/KantineSimulatie.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class KantineSimulatie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class KantineSimulatie {
+ private Kantine kantine;
+ /**
+ * Constructor
+ */
+ public KantineSimulatie() {
+ kantine=new Kantine();
+ }
+ /**
+ * Deze methode simuleert een aantal dagen in het
+ * verloop van de kantine
+ * @param dagen
+ */
+public void simuleer(int dagen) {
+ // for lus voor dagen
+ for(...) {
+ // per dag nu even vast 10+i personen naar binnen
+ // laten gaan, wordt volgende week veranderd...
+ // for lus voor personen
+ for(int j=0;j<10+i;j++){
+ // kantine.(…);
+ }
+ // verwerk rij voor de kassa
+ // toon dagtotalen (artikelen en geld in kassa)
+ // reset de kassa voor de volgende dag
+ }
+ }
+}
