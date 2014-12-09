e9cef0daad13e5dddcf94803dd5e781a1af6efb7
jeroen bakker <jeroen.henk.bakker@hccnet.nl>
2014-12-09 15:11:03 +0100

    9-12-2014

---
+++ Project kantine/Administratie.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Administratie here.
+ * 
+ * @author (Jeroen Bakker en Bas de Ruiter) 
+ * @version (1.0)
+ */
+public class Administratie
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Deze methode berekent van de int array aantal de
+     * gemiddelde waarde
+     * @param aantal
+     * @return het gemiddelde
+     */
+    public double berekenGemiddeldAantal(int[] aantal) {
+        //omitted
+        return 0;
+    }
+    
+    /**
+     * Deze methode berekent van de double array omzet de
+     * gemiddede waarde
+     * @param omzet
+     * @return Het gemiddelde
+     */
+    public double berekenGemiddeldeOmzet(double[] omzet) {
+        //omitted
+        return 0;
+    }
+    
+}
